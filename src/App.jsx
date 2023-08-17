bb54ba0a2c52a25b6eb8db59be660d61037b24d8
Yaroslav <yomelianenkowork@gmail.com>
2023-08-17 13:14:39 +0300

    module-1-lesson-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
 import Link from "./components/Link";
+import UsersList from "./components/UsersList";
+import usersJson from "./data/users.json";
+
+const title = null;
+
+// title && "No header" =>
+// title || 'No header' => 'No header'
+
+// const usersJson = [];
 
 const App = () => {
+  console.log("App");
   return (
     <>
       <Link alt="home" href="/" text="Home" />
       <br />
       <Link alt="about" href="/about" text="About" />
+      {!!usersJson.length && <UsersList title={title} users={usersJson} />}
     </>
   );
 };
+
+// Header({ title: title})
+
+// function sum(a, b) {
+//   return a + getRandomNumber(b)
+// }
+
+// function getRandomNumber(num) {
+//   return num
+// }
+
+// sum(4, 12)
 
 // Link({ alt: "home", href: "/", tetx: "Home" });
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+const Header = ({title}) => {
+  return (
+    <h1>{title}</h1>
+  )
+}
+ 
+export default Header
