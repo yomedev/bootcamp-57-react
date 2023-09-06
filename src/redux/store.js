7b76749d586008fa2b0f74194b67cc94605ab391
Yaroslav <yomelianenkowork@gmail.com>
2023-09-06 11:43:35 +0300

    module-7-redux-middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,21 +2,27 @@ import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { rootReducer } from "./reducer";
 import { getArticles } from "../services/articlesServices";
+import thunk from "redux-thunk";
 
-const middleware = (store) => (next) => (action) => {
-  console.log(action);
-  if (typeof action === "function") {
-    action(store.dispatch)
-  }
-  return next(action);
-};
+// const middleware = (store) => (next) => (action) => {
+//   console.log(action);
+//   if (typeof action === "function") {
+//     action(store.dispatch);
+//   }
+//   return next(action);
+// };
 
 export const getArticlesThunk = () => async (dispatch) => {
-  const data = await getArticles();
-  dispatch({ type: 'setArticles', payload: data})
+  dispatch({ type: "setArtclesLoading" });
+  try {
+    const data = await getArticles();
+    dispatch({ type: "setArticlesSuccess", payload: data });
+  } catch (error) {
+    dispatch({ type: "setArticlesError", payload: error });
+  }
 };
 
 export const store = createStore(
   rootReducer,
-  composeWithDevTools(applyMiddleware(middleware))
+  composeWithDevTools(applyMiddleware(thunk))
 );
